97c376fb7935513863be44769266f73cc017fe2c
ChengZY <120839275@qq.com>
2024-05-28 16:20:28 +0800

    comparison

---
+++ eval/comparison.py
@@ -0,0 +1,44 @@
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+scene = 'bed'
+lvl = 0
+path_gt = "/home/zhongyao/dl/LangTriplane/output/3d_ovs_8/{}/retrain_lang_8_{}/train/ours_None/gt".format(scene,lvl)
+path1 = "/home/zhongyao/dl/LangTriplane/output/3d_ovs_8/{}/retrain_lang_8_{}/train/ours_None/renders".format(scene,lvl)
+path2 = "/home/zhongyao/dl/LangTriplane/output/3d_ovs_8/{}/retrain_lang_8_tp_{}/train/ours_None/renders".format(scene,lvl)
+
+files = os.listdir(path1)
+file  = files[0]
+
+
+for file in files:
+    gts = []
+    comp1 = []
+    comp2 = []
+    for lvl in range(4):
+        path_gt = "/home/zhongyao/dl/LangTriplane/output/3d_ovs_8/{}/retrain_lang_8_{}/train/ours_None/gt".format(scene,
+                                                                                                                  lvl)
+        path1 = "/home/zhongyao/dl/LangTriplane/output/3d_ovs_8/{}/retrain_lang_8_{}/train/ours_None/renders".format(scene,
+                                                                                                                     lvl)
+        path2 = "/home/zhongyao/dl/LangTriplane/output/3d_ovs_8/{}/retrain_lang_8_tp_{}/train/ours_None/renders".format(
+            scene, lvl)
+
+        # file  = files[0]
+        im_gt = cv2.imread(os.path.join(path_gt,file))
+        gt = cv2.imread(os.path.join(path_gt,file))
+        im1 = cv2.imread(os.path.join(path1,file))
+        im2 = cv2.imread(os.path.join(path2,file))
+        gts.append(im_gt)
+        comp1.append(im1)
+        comp2.append(im2)
+
+gts = np.concatenate(gts,axis = 1)
+comp1 = np.concatenate(comp1,axis = 1)
+comp2 = np.concatenate(comp2,axis = 1)
+comp = np.concatenate([gts, comp1,comp2],axis = 0)
+plt.imshow(comp)
+plt.show()
+
+a = 0
